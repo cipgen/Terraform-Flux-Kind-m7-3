fa4d3d2642a3823c56a651d58013a3f6236cf705
Yuriy S <shkvarenko@gmail.com>
2024-01-06 00:38:43 -0500

    finish

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "github_repository" {
   github_token             = var.GITHUB_TOKEN
   repository_name          = var.FLUX_GITHUB_REPO
   public_key_openssh       = module.tls_private_key.public_key_openssh
-  public_key_openssh_title = "flux0"
+  public_key_openssh_title = "flux1"
 }
 
 module "tls_private_key" {
@@ -23,4 +23,3 @@ module "flux_bootstrap" {
   private_key       = module.tls_private_key.private_key_pem
   config_path       = module.kind_cluster.kubeconfig
 }
-
